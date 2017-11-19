982995a8ca9988956237ab49ea0b69544dc7602e
cherkavi <technik7job@gmail.com>
2017-11-19 22:38:22 +0100

    template with markup language

--- codeschool/main_app/views.py
+++ codeschool/main_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Person:
+
+	def __init__(self, name, surname, age):
+		self.__name=name
+		self.__surname=surname
+		self.__age=age
+
+	def name(self):
+		return self.__name
+
+	def surname(self):
+		return self.__surname
+
+	def age(self):
+		return self.__age
+
+
 def index(request):
-    name = 'Eva'
-    surname = 'Brown'
-    context = {'name': name, 'surname': surname}
-    return render(request, 'index.html', context)
+	eva = Person('Eva', 'Brown', 35)
+	rudolf = Person('Rudolf', 'Diesel', 50)
+	michael = Person('Michael', 'Osner', 28)
+	people = [eva, rudolf, michael]
+	return render(request, 'index.html', {'people':people})
