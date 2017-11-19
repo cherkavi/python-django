fe21dc8dea3e424d8343ef35e9ac6f672fb81412
cherkavi <technik7job@gmail.com>
2017-11-19 19:17:13 +0100

    map url to view

--- codeschool/codeschool/urls.py
+++ codeschool/codeschool/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from main_app import views as main_app_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', main_app_views.index),
 ]
