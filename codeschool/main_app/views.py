05d6c149cb172631cb68188d381ac1f968859fdb
cherkavi <technik7job@gmail.com>
2017-11-19 20:49:11 +0100

    extend template with context parameters from function

--- codeschool/main_app/views.py
+++ codeschool/main_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    name = 'Eva'
+    surname = 'Brown'
+    context = {'name': name, 'surname': surname}
+    return render(request, 'index.html', context)
